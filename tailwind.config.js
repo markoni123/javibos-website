8c452573d920cd06d3e4c20aa10eb21b0011c373
markoni123 <marko.cpk@my.com>
2025-04-08 02:10:42 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        fadeInDown: {
+          '0%': { opacity: 0, transform: 'translateY(-20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out',
+        'fade-in-down': 'fadeInDown 1s ease-out'
+      }
+    }
+  },
   plugins: [],
 }
